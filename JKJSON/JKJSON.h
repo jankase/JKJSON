ec13fe2c602453bde1aa29653d2cbbc4b688eb52
Jan Kase <jan.kase@me.com>
2015-12-23 23:46:20 +0100

    Initial Commit

---
+++ JKJSON/JKJSON.h
@@ -0,0 +1,19 @@
+//
+//  JKJSON.h
+//  JKJSON
+//
+//  Created by Jan Kase on 23/12/15.
+//  Copyright © 2015 Jan Kase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JKJSON.
+FOUNDATION_EXPORT double JKJSONVersionNumber;
+
+//! Project version string for JKJSON.
+FOUNDATION_EXPORT const unsigned char JKJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JKJSON/PublicHeader.h>
+
+
